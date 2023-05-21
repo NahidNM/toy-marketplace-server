bf72e3e05d921755b4cdb8cf0f4ea5eb23313487
NahidNM <mdnahidhasan171@gmail.com>
2023-05-21 21:38:48 +0600

    server added

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+     client.connect();
 
 // const toyCollection = client.db('toyMarket').collection('myToys');
 const allToyCollection = client.db('toyMarket').collection('allToys');
